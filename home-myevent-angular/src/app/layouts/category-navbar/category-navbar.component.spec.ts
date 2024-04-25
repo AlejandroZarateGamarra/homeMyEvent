9b11db133df9617fc81fad9395e12ac7722dfdce
Alejandro Zárate <102533262+AlejandroZarateGamarra@users.noreply.github.com>
2024-04-25 17:03:57 -0500

    Add home view component

--- home-myevent-angular/src/app/layouts/category-navbar/category-navbar.component.spec.ts
+++ home-myevent-angular/src/app/layouts/category-navbar/category-navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { CategoryNavbarComponent } from './category-navbar.component';
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+describe('CategoryNavbarComponent', () => {
+  let component: CategoryNavbarComponent;
+  let fixture: ComponentFixture<CategoryNavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [CategoryNavbarComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(CategoryNavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
